2b9552c1c7aef70a3660a0264b25138ddf9f513c
aywhoosh <145834240+aywhoosh@users.noreply.github.com>
2025-08-18 19:00:49 +0000

    Deploying to gh-pages from @ aywhoosh/aywhoosh.github.io@69115804e3a408f179bb82948ef5547db267b2b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,13 +511,6 @@ ninja.data = [{
           window.open("https://github.com/aywhoosh", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
@@ -532,18 +525,11 @@ ninja.data = [{
           window.open("https://medium.com/@by.ayushjain", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://aywhoosh.github.io", "_blank");
         },
       },{
       id: 'light-theme',
