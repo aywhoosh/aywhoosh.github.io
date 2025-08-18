ea1dd6eb9f6479da7b0c19f568fb28c7d02d24d6
aywhoosh <145834240+aywhoosh@users.noreply.github.com>
2025-08-18 15:03:07 +0000

    Deploying to gh-pages from @ aywhoosh/aywhoosh.github.io@37660d377bf59117c837c88dd956a6b0a596208b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%79.%61%79%75%73%68%6A%61%69%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/aywhoosh", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -511,11 +518,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/byayushjain", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@by.ayushjain", "_blank");
         },
       },{
         id: 'social-scholar',
